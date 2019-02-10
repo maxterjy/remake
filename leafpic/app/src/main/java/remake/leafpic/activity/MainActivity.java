d4a4a000e11ca93a354ff7e92c562558dfe1cbbc
maxterjy <maxterjy@gmail.com>
2019-02-10 15:59:25 +0700

    [LeafPic] first commit

---
+++ leafpic/app/src/main/java/remake/leafpic/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package remake.leafpic.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import remake.leafpic.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
