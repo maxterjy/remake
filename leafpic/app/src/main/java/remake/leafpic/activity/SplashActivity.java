dc1b6d326776548b12c68e8aa3ee6b4d1cc3b7f2
maxterjy <maxterjy@gmail.com>
2019-02-10 16:25:07 +0700

    [LeafPic][SlashActivity] Enable fullscreen mode

--- leafpic/app/src/main/java/remake/leafpic/activity/SplashActivity.java
+++ leafpic/app/src/main/java/remake/leafpic/activity/SplashActivity.java
@@ -2,6 +2,8 @@ package remake.leafpic.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
 import remake.leafpic.R;
 
 public class SplashActivity extends AppCompatActivity {
@@ -10,5 +12,10 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
     }
 }
