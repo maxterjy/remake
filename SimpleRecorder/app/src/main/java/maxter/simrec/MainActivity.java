6f8eeb5686b14a3c88a4cd19010d452d277d4bde
maxterjy <maxterjy@gmail.com>
2019-04-12 08:27:35 +0700

    [SimpleRecorder] Add ViewPager, TabLayout to MainActivity

--- SimpleRecorder/app/src/main/java/maxter/simrec/MainActivity.java
+++ SimpleRecorder/app/src/main/java/maxter/simrec/MainActivity.java
@@ -1,15 +1,66 @@
 package maxter.simrec;
 
 import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    ViewPager mViewPager;
+    TabLayout mTabLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mViewPager = findViewById(R.id.viewpager);
+        mViewPager.setAdapter(new MainPagerAdapter(getSupportFragmentManager()));
+
+        mTabLayout = findViewById(R.id.tabLayout);
+        mTabLayout.setupWithViewPager(mViewPager);
     }
+
+
+    public class MainPagerAdapter extends FragmentPagerAdapter {
+        String mTitles[] = {"RECORD", "FILES"};
+
+        public MainPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            switch (i){
+                case 0:{
+                    return new RecordFragment();
+                }
+
+
+                case 1:{
+                    return new FileViewerFragment();
+                }
+            }
+
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return mTitles.length;
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mTitles[position];
+        }
+    }//end MainPagerAdapter
 }
